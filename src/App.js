631d7dc72c1851e0a5e88dc54e31e1d5b7fadd8c
Abiud Orina <abiudrn@gmail.com>
2023-10-01 22:04:00 +0300

    refactor: pass props to the square component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
 
-function Square() {
-  return <button className="square">1</button>;
+function Square({ val }) {
+  // A prop called val
+  return <button className="square">{val}</button>;
 }
 
 export default function Board() {
-  return(
+  return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square val="1" />
+        <Square val="2" />
+        <Square val="3" />
       </div>
       <div className="board-row">
-      <Square />
-        <Square />
-        <Square />
+        <Square val="4" />
+        <Square val="5" />
+        <Square val="6" />
       </div>
       <div className="board-row">
-      <Square />
-        <Square />
-        <Square />
+        <Square val="7" />
+        <Square val="8" />
+        <Square val="9" />
       </div>
     </>
-  )
+  );
 }
-
