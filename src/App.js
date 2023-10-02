2ccf3537e56c0801f3d6221b21bd5aefe3049a90
Abiud Orina <abiudrn@gmail.com>
2023-10-02 09:25:39 +0300

    feat: dynamically update the squares with centralized state management

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React from 'react';
 import { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-
+function Square({ val, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
-      {value}
+    <button className="square" onClick={onSquareClick}>
+      {val}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square val={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square val={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square val={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square val={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square val={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square val={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square val={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square val={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square val={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
